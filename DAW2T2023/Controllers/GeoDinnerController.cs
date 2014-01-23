60924264553f64cc9d4b44b065ac4df4564db2dd
Marco <marcoxavibsc@gmail.com>
2014-01-23 08:59:24 -0500

    Proyecto Inicio

---
+++ DAW2T2023/Controllers/GeoDinnerController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAW2T2023.Controllers
+{
+    public class GeoDinnerController : Controller
+    {
+        //
+        // GET: /GeoDinner/
+        
+        public ActionResult Index()
+        {
+            ViewBag.accion = RouteData.Values["action"];
+            return View();
+        }
+
+        public ActionResult Restaurante(String value="Default")
+        {
+            ViewBag.accion = RouteData.Values["action"];
+            ViewBag.valor = value;
+            return View();
+        }
+
+        //
+        // GET: /GeoDinner/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /GeoDinner/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /GeoDinner/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /GeoDinner/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /GeoDinner/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /GeoDinner/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /GeoDinner/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DAW2T2023/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DAW2T2023.MvcApplication" Language="C#" %>
